0806787bf4f35a37159b5b3b10af4cb9e91ea900
Naymul Hasan Fahim <imfahim05@gmail.com>
2020-08-24 10:53:58 +0600

    All Done

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Cart = (props) => {
+    const cart = props.cart;
+    // const total = cart.reduce((total,prd)=>total+prd.price,0)
+    let total = 0;
+    for (let i = 0; i < cart.length; i++) {
+        const product = cart[i];
+        total = total + product.price;
+        
+    }
+    let shipping = 0;
+    if(total>15){
+        shipping = 4.99;
+    }
+    else if(total>0){
+        shipping = 12.99;
+    }
+    const tax = (total/10).toFixed(2);
+    const grandTotal = (total+shipping+Number(tax)).toFixed(2);
+    return (
+        <div>
+                <h4>order summery</h4>
+                <p>items ordered: {cart.length}</p>
+                <p><small>Shipping cost: {shipping}</small></p>
+                <p>product cost:{total}</p>
+                <p>Tax + vat:{tax}</p>
+                <p>Total price: {grandTotal}</p>
+        </div>
+    );
+};
+
+export default Cart;
---
+++ src/components/Product/Product.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee,faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import './Product.css';
+
+const Product = (props) => {
+    // console.log(props)
+const { name, img, seller, price, stock } = props.product;
+return(
+    <div className="product">
+        <div>
+            <img src={img} alt="" />
+        </div>
+        <div>
+            <h4 className="product-name">{name}</h4>
+            <br />
+            <p><small> by:{seller}</small></p>
+            <br />
+            <p>${price}</p>
+            <br />
+            <p><small>Only {stock} left in stock-order soon </small></p>
+            <button onClick={()=>props.handleAddProduct(props.product)} className="add-button"><FontAwesomeIcon icon={faShoppingCart} />Add to cart</button>
+        </div>
+    </div>
+    );
+}
+            
+            
+             
+            
+            
+export default Product;
